e375d83a5d1cded2fe26c26155bc03f537522b0f
Yogesh Jayachandran <108916534+Yogeshpj@users.noreply.github.com>
2023-04-05 14:26:28 +0530

    Add files via upload

---
+++ src/Singers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Singers() {
+  return (
+    <div>
+        <h4>Anirudh Ravichandar</h4>
+        <h4>2014</h4>
+        <h4>Harris Jayaraj</h4>
+        <h4>1993</h4>
+        <h4>AR Rahman</h4>
+        <h4>1989</h4>
+        <h4>Arjit Singh</h4>
+        <h4>1989</h4>
+    </div>
+  )
+}
+
+export default Singers
